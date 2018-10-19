e50ebb75a83d98f8488908123ff8a511ee10185c
AnActualEndlessVoid <42186898+AnActualEndlessVoid@users.noreply.github.com>
2018-10-19 13:37:12 -0500

    added the counters

--- main.c
+++ main.c
@@ -13,6 +13,12 @@ int main(void) {
 
     char arr[100];
     int i;
+    int alfabetos = 0;
+    int vocales = 0;
+    int consonantes = 0;
+    int digitos = 0;
+    int espacios = 0;
+    int especiales = 0;
 
     //Obteniendo el string del usuario
     printf ("Give me a series of characters:");
@@ -25,33 +31,43 @@ int main(void) {
         if ((arr[i]>='a' && arr[i]<='z') || (arr[i]>='A' && arr[i]<='Z'))
         {
             printf ("%c is an alphabet\n", arr[i]);
+            alfabetos++;
         //Estableciendo los valores para que se considere vowel
             if ((arr[i]=='a')||(arr[i]=='e')||(arr[i]=='i')||(arr[i]=='o')||(arr[i]=='u')||(arr[i]=='A')||(arr[i]=='E')||(arr[i]=='I')||(arr[i]=='O')||(arr[i]=='U'))
             {
                 printf ("\t%c is a vowel\n", arr[i]);
+                vocales++;
             }
             //Si no es vowel, es consonant
             else
             {
                 printf ("\t%c is a consonant\n", arr[i]);
+                consonantes++;
             }
             //Estableciendo los valores para que se considere digit
         }
         else if ((arr[i]>='0')&&(arr[i]<='9'))
         {
             printf ("%c is a digit \n", arr[i]);
+            digitos++;
         }
         //Estableciendo el valor del espacio
         else if (arr[i]==' ')
         {
             printf ("%c is a space \n", arr[i]);
+            espacios++;
         }
         //Si no es ninguna otra clasificacion, es caracter especial
         else
         {
             printf ("%c is a special character\n", arr[i]);
+            especiales++;
         }
-
+    printf ("There are %i alphabets in the string\n",alfabetos);
+        printf ("\t Out of which %i are vowels, and %i are consonants\n", vocales,consonantes);
+        printf ("There are %i digits in the string \n", digitos);
+        printf ("There are %i spaces in the string\n", espacios);
+        printf ("There are %i special characters in the string\n", especiales);
     }
     return 0;
 }
